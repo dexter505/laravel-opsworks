3f9f4e7b982c5015965b34c84aee79cb063cf7da
Dexter <dexter505@users.noreply.github.com>
2016-01-23 23:03:49 -0800

    Update install.rb

--- phantomjs/recipes/install.rb
+++ phantomjs/recipes/install.rb
@@ -2,8 +2,8 @@ node[:deploy].each do |application, deploy|
   script "install_phantomjs" do
     interpreter "bash"
     user "root"
-    cwd "#{deploy[:deploy_to]}/current"
-    code <<-EOH
+    #cwd "#{deploy[:deploy_to]}/current"
+    #code <<-EOH
     apt-get install phantomjs
     EOH
   end
