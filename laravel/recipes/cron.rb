8bce3c13001d758037ca73c5006dca8b21e29d01
Dexter <dexter505@users.noreply.github.com>
2016-01-24 22:19:58 -0800

    Update cron.rb

--- laravel/recipes/cron.rb
+++ laravel/recipes/cron.rb
@@ -1,5 +1,5 @@
 node[:deploy].each do |application, deploy|
   cron "laravel_tasks" do
-    command "cd /srv/www/myapp/current && php artisan schedule:run >> /dev/null 2>&1"
+    command "cd #{deploy[:deploy_to]}/current && php artisan schedule:run >> /dev/null 2>&1"
   end
 end
